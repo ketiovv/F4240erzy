bfa32392b4719f53d0b0fcc193df6c88dc57142c
Wojtek <wojtek.kajstura@gmail.com>
2020-12-18 15:47:49 +0100

    created sample.sql for seeding values to db

---
+++ db/sample.sql
@@ -0,0 +1,9 @@
+insert into tutorials(title, description, published, createdAt, updatedAt) 
+values  
+('tutorial10','desc10', false, NOW(), NOW()),
+('tutorial11','desc11', false, NOW(), NOW()),
+('tutorial12','desc12', false, NOW(), NOW()),
+('tutorial13','desc13', false, NOW(), NOW()),
+('tutorial14','desc14', false, NOW(), NOW()),
+('tutorial15','desc15', false, NOW(), NOW()),
+('tutorial16','desc16', false, NOW(), NOW());
