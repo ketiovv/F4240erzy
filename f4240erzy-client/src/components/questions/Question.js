1486f04564afb7539e3e65c6ca635dd9dc1d7467
Wojtek <wojtek.kajstura@gmail.com>
2021-01-12 10:49:26 +0100

    styled question

--- f4240erzy-client/src/components/questions/Question.js
+++ f4240erzy-client/src/components/questions/Question.js
@@ -5,17 +5,22 @@ import './Question.css';
 const Question = ({question}) => {
     if (question == null) {
         return(
-            <div className="current-question">
-                <h1>bla bla bla bla bla</h1>
+            <div className="container">
+                <div className="current-question">      
+                    <h1>Trwa ładowanie pytania..</h1>
+                </div>
             </div>
+            
         )
     }
     else{
         return(
-            <div className="current-question">
-                <h1>
-                    {question.contentOfQuestion}
-                </h1>
+            <div className="container-fluid">
+                <div className="current-question">
+                    <p>
+                        {question.contentOfQuestion}
+                    </p>
+                </div>
             </div>
         )
     }
