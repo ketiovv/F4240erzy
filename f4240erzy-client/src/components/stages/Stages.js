5395176b4d866bdb55ad4335fdfdc0f92f2d031f
Wojtek <wojtek.kajstura@gmail.com>
2021-01-12 10:49:19 +0100

    styled stages

--- f4240erzy-client/src/components/stages/Stages.js
+++ f4240erzy-client/src/components/stages/Stages.js
@@ -1,18 +1,17 @@
 import React from 'react'
+import "./Stage.css"
 
 // function component
-const Stages = ({stages,currentStageNumber}) => {
+const Stages = ({stages, currentStageNumber}) => {
     return (
-        <div>{
+        <div className="stages-background">{
             stages.map((stage) => (
-                <div className={ stage.stageNumber != currentStageNumber ? "card" : "card text-white bg-secondary" }>
-                    <div className="card-body">
-                        <h5 className="card-title">
-                            Stage #{stage.stageNumber}
-                        </h5>
-                        <h6 className="card-subtitle mb-2">
-                            Amount: {stage.amount}
-                        </h6>
+                <div className={ stage.stageNumber != currentStageNumber ? "row stage" : "row stage active" }>
+                    <div className="col-1 stage-number">
+                        {stage.stageNumber}
+                    </div>
+                    <div className="col-9 stage-amount">
+                        {stage.amount == 1000000 ? "F4240" : stage.amount} zł
                     </div>
                 </div>
             ))}
